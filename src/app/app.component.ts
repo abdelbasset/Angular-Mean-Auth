2403d888e8ad555af58a55dbf7aa095372279bc6
Abdelbasset <abagga@elyosdigital.com>
2020-01-20 12:41:33 +0100

    Upload Project Auth

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { AuthService } from './shared/auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-meanstack-authentication';
+
+  constructor(public authService: AuthService) { }
+
+  logout() {
+    this.authService.doLogout()
+  }
+}
